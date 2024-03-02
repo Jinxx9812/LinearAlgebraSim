8ed17df3c0bacad75c8db828f0c6baf509cc5d74
Diego Guzmán <diego.e.guz90@gmail.com>
2024-03-02 10:48:55 -0500

    Scene cross product added, scene quaternions (just draw 3d cube)

---
+++ Assets/Scripts/DrawCube.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCube : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private Vector3 center = new();
+
+    private Vector3[] vertices = new Vector3[]
+    {        
+        new Vector3(-0.5f, -0.5f, -0.5f), // Vertex 0
+        new Vector3(0.5f, -0.5f, -0.5f),  // Vertex 1
+        new Vector3(0.5f, -0.5f, 0.5f),   // Vertex 2
+        new Vector3(-0.5f, -0.5f, 0.5f),  // Vertex 3
+        new Vector3(-0.5f, 0.5f, -0.5f),  // Vertex 4
+        new Vector3(0.5f, 0.5f, -0.5f),   // Vertex 5
+        new Vector3(0.5f, 0.5f, 0.5f),    // Vertex 6
+        new Vector3(-0.5f, 0.5f, 0.5f)    // Vertex 7
+
+    };
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        RestartCubePosition();
+        DrawCube3D();
+    }
+
+    private void RestartCubePosition()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(-0.5f, -0.5f, -0.5f), // Vertex 0
+            new Vector3(0.5f, -0.5f, -0.5f),  // Vertex 1
+            new Vector3(0.5f, -0.5f, 0.5f),   // Vertex 2
+            new Vector3(-0.5f, -0.5f, 0.5f),  // Vertex 3
+            new Vector3(-0.5f, 0.5f, -0.5f),  // Vertex 4
+            new Vector3(0.5f, 0.5f, -0.5f),   // Vertex 5
+            new Vector3(0.5f, 0.5f, 0.5f),    // Vertex 6
+            new Vector3(-0.5f, 0.5f, 0.5f)    // Vertex 7
+
+        };
+        center = transform.position;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = vertices[i] + center;
+        }
+    }
+
+    private void DrawCube3D()
+    {
+        // vertex on linerenderer
+        lineRenderer.positionCount = vertices.Length;
+        lineRenderer.SetPositions(vertices);
+
+        // define lines that conect vertex
+        int[] cubeIndex = new int[]
+        {
+            0,1,1,2,2,3,3,0, // bottom cube face
+            4,5,5,6,6,7,7,4, // upper cube face
+            0,4,1,5,2,6,3,7
+        };
+
+        // draw lines
+        lineRenderer.positionCount = cubeIndex.Length;
+        for(int i = 0; i < cubeIndex.Length; i++)
+        {
+            lineRenderer.SetPosition(i, vertices[cubeIndex[i]]);
+        }
+    }
+}
